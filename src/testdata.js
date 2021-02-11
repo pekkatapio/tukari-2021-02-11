56080f0e3480b4d4eba5ab9b4b7204c5c5131894
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2021-02-11 13:15:10 +0200

    testdata added

---
+++ src/testdata.js
@@ -0,0 +1,24 @@
+const tanktank = [
+    { id: "1234-3455",
+      paiva: "2021-02-11",
+      lukema: 83566,
+      summa: 64.74 },
+    { id: "7856-3734",
+      paiva: "2021-02-01",
+      lukema: 83066,
+      summa: 59.74 },
+    { id: "9347-3778",
+      paiva: "2021-01-24",
+      lukema: 82500,
+      summa: 68.34 },
+    { id: "9283-3234",
+      paiva: "2021-01-17",
+      lukema: 82050,
+      summa: 57.12 },
+    { id: "8347-2345",
+      paiva: "2021-01-06",
+      lukema: 81750,
+      summa: 68.32 }
+  ]
+
+export default tanktank;
